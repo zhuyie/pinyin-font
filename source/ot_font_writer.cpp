c66f4a6c916808f5a7f889de62fef3aef6434faf
zhuyie <zhuyie@gmail.com>
2022-05-20 17:41:45 +0800

    Write head/maxp table

--- source/ot_font_writer.cpp
+++ source/ot_font_writer.cpp
@@ -7,7 +7,7 @@
 //------------------------------------------------------------------------------
 
 OpenType_Font_Writer::OpenType_Font_Writer()
-: font_(NULL)
+: font_(NULL), checksumAdjustment_(NULL)
 {
 }
 
@@ -29,9 +29,45 @@ Status OpenType_Font_Writer::Write(const char *filename, const OpenType_Font *fo
         return kFileError;
     auto f_guard = scopeGuard([&f]{ fclose(f); });
 
-    if ((status = __writeFileHeader(f, 10)) != kOk)
-        return status;
+    uint16_t glyphCount = font_->GlyphCount();
+    if (glyphCount < 1000) {
+        buf_.reserve(128 * 1024);
+    } else if (glyphCount < 2500) {
+        buf_.reserve(256 * 1024);
+    } else if (glyphCount < 5000) {
+        buf_.reserve(1 * 1024 * 1024);
+    } else if (glyphCount < 10000) {
+        buf_.reserve(2 * 1024 * 1024);
+    } else {
+        buf_.reserve(4 * 1024 * 1024);
+    }
 
+    uint16_t numTables = 10;
+    if ((status = __writeFileHeader(numTables)) != kOk)
+        return status;
+    if ((status = __writeTableHead(0)) != kOk)
+        return status;
+    if ((status = __writeTableMaxp(1)) != kOk)
+        return status;
+    if ((status = __writeTablePost(2)) != kOk)
+        return status;
+    if ((status = __writeTableOS2(3)) != kOk)
+        return status;
+    if ((status = __writeTableName(4)) != kOk)
+        return status;
+    if ((status = __writeTableLocaGlyf(5)) != kOk)
+        return status;
+    if ((status = __writeTableHhea(7)) != kOk)
+        return status;
+    if ((status = __writeTableHmtx(8)) != kOk)
+        return status;
+    if ((status = __writeTableCmap(9)) != kOk)
+        return status;
+    __updateChecksumAdjustment(numTables);
+
+    if (fwrite(&(buf_[0]), 1, buf_.size(), f) != buf_.size()) {
+        return kFileError;
+    }
     if (fflush(f) != 0) {
         return kFileError;
     }
@@ -39,29 +75,164 @@ Status OpenType_Font_Writer::Write(const char *filename, const OpenType_Font *fo
     return kOk;
 }
 
-Status OpenType_Font_Writer::__writeFileHeader(FILE *f, uint16_t numTables)
+Status OpenType_Font_Writer::__writeFileHeader(uint16_t numTables)
 {
+    buf_.resize(12 + 16 * numTables);
+    uint8_t *b = &(buf_[0]);
+
     // https://docs.microsoft.com/en-us/typography/opentype/spec/otff#table-directory
-    uint8_t buf[12 + 16] = { 0 };
     uint32_t sfntVersion = 0x00010000;
     uint16_t entrySelector = (uint16_t)(std::floor(std::log2(numTables)));
     uint16_t searchRange = (uint16_t)(std::exp2(entrySelector) * 16);
     uint16_t rangeShift = numTables * 16 - searchRange;
 
-    put_u4(buf + 0,  sfntVersion);
-    put_u2(buf + 4,  numTables);
-    put_u2(buf + 6,  searchRange);
-    put_u2(buf + 8,  entrySelector);
-    put_u2(buf + 10, rangeShift);
-    if (fwrite(buf, 1, 12, f) != 12) {
-        return kFileError;
-    }
-
-    for (uint16_t i = 0; i < numTables; i++) {
-        if (fwrite(buf + 12, 1, 16, f) != 16) {
-            return kFileError;
-        }
-    }
+    put_u4(b + 0,  sfntVersion);
+    put_u2(b + 4,  numTables);
+    put_u2(b + 6,  searchRange);
+    put_u2(b + 8,  entrySelector);
+    put_u2(b + 10, rangeShift);
 
     return kOk;
+}
+
+uint32_t OpenType_Font_Writer::__checksum(const uint8_t *buf, uint32_t length)
+{
+    assert (0 == (length & 3));
+    const uint32_t *table = (const uint32_t*)buf;
+    const uint32_t *tableEnd = table + length / 4;
+    uint32_t sum = 0;
+    while (table < tableEnd) {
+        sum += *table;
+        table++;
+    }
+    return sum;
+}
+
+Status OpenType_Font_Writer::__writeTableHead(uint16_t tableIndex)
+{
+    size_t offset = buf_.size();
+    buf_.resize(offset + 56);  // 54 + 2
+
+    uint8_t *b = &(buf_[offset]);
+    const OpenType_Head &head = font_->head_;
+    put_u4(b + 0,  head.Version);
+    put_u4(b + 4,  head.FontRevision);
+    put_u4(b + 8,  0);
+    put_u4(b + 12, head.MagicNumber);
+    put_u2(b + 16, head.Flags);
+    put_u2(b + 18, head.UnitsPerEm);
+    put_u8(b + 20, head.Created);
+    put_u8(b + 28, head.Modified);
+    put_i2(b + 36, head.XMin);
+    put_i2(b + 38, head.YMin);
+    put_i2(b + 40, head.XMax);
+    put_i2(b + 42, head.YMax);
+    put_u2(b + 44, head.MacStyle);
+    put_u2(b + 46, head.LowestRecPPEM);
+    put_i2(b + 48, head.FontDirectionHint);
+    put_i2(b + 50, 1);  // Always use long format
+    put_i2(b + 52, head.GlyphDataFormat);
+
+    uint8_t *t = &(buf_[12 + tableIndex * 16]);
+    memcpy(t, "head", 4);
+    put_u4(t + 4,  __checksum(b, 56));
+    put_u4(t + 8,  offset);
+    put_u4(t + 12, 54);
+
+    checksumAdjustment_ = b + 8;
+
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTableMaxp(uint16_t tableIndex)
+{
+    // https://docs.microsoft.com/en-us/typography/opentype/spec/maxp
+    // Fonts with TrueType outlines must use Version 1.0 of this table
+
+    size_t offset = buf_.size();
+    buf_.resize(offset + 32);
+
+    uint8_t *b = &(buf_[offset]);
+    const OpenType_Maxp &maxp = font_->maxp_;
+    put_u4(b + 0,  0x00010000);
+    put_u2(b + 4,  maxp.NumGlyphs);
+    put_u2(b + 6,  maxp.MaxPoints);
+    put_u2(b + 8,  maxp.MaxContours);
+    put_u2(b + 10, maxp.MaxCompositePoints);
+    put_u2(b + 12, maxp.MaxCompositeContours);
+    put_u2(b + 14, maxp.MaxZones);
+    put_u2(b + 16, maxp.MaxTwilightPoints);
+    put_u2(b + 18, maxp.MaxStorage);
+    put_u2(b + 20, maxp.MaxFunctionDefs);
+    put_u2(b + 22, maxp.MaxInstructionDefs);
+    put_u2(b + 24, maxp.MaxStackElements);
+    put_u2(b + 26, maxp.MaxSizeOfInstructions);
+    put_u2(b + 28, maxp.MaxComponentElements);
+    put_u2(b + 30, maxp.MaxComponentDepth);
+
+    uint8_t *t = &(buf_[12 + tableIndex * 16]);
+    memcpy(t, "maxp", 4);
+    put_u4(t + 4,  __checksum(b, 32));
+    put_u4(t + 8,  offset);
+    put_u4(t + 12, 32);
+
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTablePost(uint16_t tableIndex)
+{
+    // TODO
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTableOS2(uint16_t tableIndex)
+{
+    // TODO
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTableName(uint16_t tableIndex)
+{
+    // TODO
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTableLocaGlyf(uint16_t tableIndex)
+{
+    // TODO
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTableHhea(uint16_t tableIndex)
+{
+    // TODO
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTableHmtx(uint16_t tableIndex)
+{
+    // TODO
+    return kOk;
+}
+
+Status OpenType_Font_Writer::__writeTableCmap(uint16_t tableIndex)
+{
+    // TODO
+    return kOk;
+}
+
+void OpenType_Font_Writer::__updateChecksumAdjustment(uint16_t numTables)
+{
+    assert(checksumAdjustment_ != NULL);
+
+    uint32_t sum = 0;
+    const uint8_t *b = &(buf_[0]);
+    uint32_t length = 12 + 16 * numTables;
+    sum = __checksum(b, length);  // checksum of table directory
+    b += length;
+    for (uint16_t i = 0; i < numTables; i++) {
+        sum += u4(b + 4);  // checksum of this table
+        b += 16;
+    }
+    *checksumAdjustment_ = sum - 0xB1B0AFBA;
 }
--- source/ot_font_writer.h
+++ source/ot_font_writer.h
@@ -2,12 +2,15 @@
 #define __PINYIN_FONT_OT_FONT_WRITER_H__
 
 #include "ot_font.h"
+#include <vector>
 
 //------------------------------------------------------------------------------
 
 class OpenType_Font_Writer
 {
     const OpenType_Font *font_;
+    std::vector<uint8_t> buf_;
+    uint8_t *checksumAdjustment_;
 
 public:
     OpenType_Font_Writer();
@@ -16,7 +19,18 @@ public:
     Status Write(const char *filename, const OpenType_Font *font);
 
 private:
-    Status __writeFileHeader(FILE *f, uint16_t numTables);
+    Status __writeFileHeader(uint16_t numTables);
+    uint32_t __checksum(const uint8_t *table, uint32_t length);
+    Status __writeTableHead(uint16_t tableIndex);
+    Status __writeTableMaxp(uint16_t tableIndex);
+    Status __writeTablePost(uint16_t tableIndex);
+    Status __writeTableOS2(uint16_t tableIndex);
+    Status __writeTableName(uint16_t tableIndex);
+    Status __writeTableLocaGlyf(uint16_t tableIndex);
+    Status __writeTableHhea(uint16_t tableIndex);
+    Status __writeTableHmtx(uint16_t tableIndex);
+    Status __writeTableCmap(uint16_t tableIndex);
+    void __updateChecksumAdjustment(uint16_t numTables);
 };
 
 //------------------------------------------------------------------------------
