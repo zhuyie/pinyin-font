2ddad678b6f7b021c7797fd0e513f5d6b475cfaa
zhuyie <zhuyie@gmail.com>
2022-05-20 10:37:11 +0800

    Add put_u1/put_u2/put_u4/put_u8

--- source/utility.h
+++ source/utility.h
@@ -12,17 +12,12 @@ inline uint8_t u1(const uint8_t *b)
 
 inline uint16_t u2(const uint8_t *b)
 {
-    return ((uint16_t)b[0] << 8) | b[1];
+    return ((uint16_t)b[0] << 8) | (uint16_t)b[1];
 }
 
 inline int16_t i2(const uint8_t *b)
 {
     return (int16_t)u2(b);
-}
-
-inline uint32_t u3(const uint8_t *b)
-{
-    return ((uint32_t)b[0] << 16) | ((uint32_t)b[1] << 8) | (uint32_t)b[2];
 }
 
 inline uint32_t u4(const uint8_t *b)
@@ -44,6 +39,52 @@ inline uint64_t u8(const uint8_t *b)
 inline int64_t i8(const uint8_t *b)
 {
     return (int64_t)u8(b);
+}
+
+inline void put_u1(uint8_t *b, uint8_t v)
+{
+    b[0] = v;
+}
+
+inline void put_u2(uint8_t *b, uint16_t v)
+{
+    b[0] = (uint8_t)(v >> 8);
+    b[1] = (uint8_t)(v);
+}
+
+inline void put_i2(uint8_t *b, int16_t v)
+{
+    put_u2(b, (uint16_t)v);
+}
+
+inline void put_u4(uint8_t *b, uint32_t v)
+{
+    b[0] = (uint8_t)(v >> 24);
+    b[1] = (uint8_t)(v >> 16);
+    b[2] = (uint8_t)(v >> 8);
+    b[3] = (uint8_t)(v);
+}
+
+inline void put_i4(uint8_t *b, int32_t v)
+{
+    put_u4(b, (uint32_t)v);
+}
+
+inline void put_u8(uint8_t *b, uint64_t v)
+{
+    b[0] = (uint8_t)(v >> 56);
+    b[1] = (uint8_t)(v >> 48);
+    b[2] = (uint8_t)(v >> 40);
+    b[3] = (uint8_t)(v >> 32);
+    b[4] = (uint8_t)(v >> 24);
+    b[5] = (uint8_t)(v >> 16);
+    b[6] = (uint8_t)(v >> 8);
+    b[7] = (uint8_t)(v);
+}
+
+inline void put_i8(uint8_t *b, int64_t v)
+{
+    put_u8(b, (uint64_t)v);
 }
 
 //------------------------------------------------------------------------------
